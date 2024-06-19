11a2402b87a522796c6b2ae9a05be0658f34884a
toryoto <s1f102002345@iniad.org>
2024-06-19 11:18:35 +0900

    ダミーデータの表示

---
+++ src/data/dummyTodoList.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todo";
+
+export const dummyTodoList: Todo[] = [
+  {
+    id: 1,
+    title: 'To study React',
+    completed: false,
+  },
+  {
+    id: 2,
+    title: 'To study next.js',
+    completed: false,
+  },
+  {
+    id: 3,
+    title: 'To develop Dapps',
+    completed: true,
+  },
+]
