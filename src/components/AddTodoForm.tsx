0201bbaeb0c64e8551630438e767324aa4d49086
toryoto <s1f102002345@iniad.org>
2024-06-20 11:16:00 +0900

    Todo追加フォームの作成

---
+++ src/components/AddTodoForm.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { Plus } from "lucide-react";
+
+type Props = {
+  addTodo: (title: string) => void;
+};
+
+export const AddTodoForm = ({ addTodo }: Props) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    // ページの更新を防ぐ
+    e.preventDefault();
+    // 親に値を渡す
+    addTodo(inputValue);
+    setInputValue('');
+  }
+
+  return (
+    <form 
+      className="flex"
+      onSubmit={onSubmit}
+    >
+      <input
+        type="text"
+        placeholder="新しいTodoを入力してください"
+        className="grow rounded-s bg-slate-200 p-2"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button
+        type="submit"
+        className="rounded-e bg-blue-600 p-2 transition-colors hover:bg-blue-800 disabled:bg-gray-400"
+        disabled={!inputValue}
+      >
+        <Plus className="text-white" />
+      </button>
+    </form>
+  );
+};
