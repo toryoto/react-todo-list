f50616842ab995c0d5e7bd53a59206778460bc8f
toryoto <s1f102002345@iniad.org>
2024-06-20 11:16:20 +0900

    Todo追加機能の実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { TodoList } from "./components/TodoList";
+import { AddTodoForm } from './components/AddTodoForm';
 import { dummyTodoList } from "./data/dummyTodoList";
 
 function App() {
@@ -20,11 +21,26 @@ function App() {
     });
   };
 
+  const addTodo = (title: string) => {
+    setTodoList((prevTodoList) => {
+      const newTodo = {
+        id: Date.now(),
+        title,
+        completed: false,
+      };
+
+      return [newTodo, ...prevTodoList];
+    });
+  };
+
   return (
     <main className="mx-auto mt-10 max-w-xl space-y-10">
       <h1 className="text-center text-4xl">Todoアプリ</h1>
-      <div className="rounded bg-slate-200 p-5">
-        <TodoList todoList={todoList} changeCompleted={changeCompleted} />
+      <div className="space-y-5">
+        <AddTodoForm addTodo={addTodo} />
+        <div className='rounded bg-slate-200 p-5'>
+          <TodoList todoList={todoList} changeCompleted={changeCompleted} />
+        </div>
       </div>
     </main>
   );
